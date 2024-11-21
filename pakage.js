80f626f3d052250712b749cc134b30472123be08
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-11-21 19:20:59 +0530

    Create pakage.js

---
+++ pakage.js
@@ -0,0 +1,21 @@
+{
+  "name": "hyper-md-whatsapp-bot",
+  "version": "1.0.0",
+  "description": "A powerful WhatsApp bot built using the Baileys library",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "test": "echo \"No tests specified\" && exit 1"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.5.0",
+    "axios": "^1.5.0",
+    "express": "^4.18.2",
+    "fs": "^0.0.1-security",
+    "megajs": "^1.0.0",
+    "pino": "^8.14.1",
+    "qrcode-terminal": "^0.12.0"
+  },
+  "author": "Mr Senesh",
+  "license": "MIT"
+      }
