dacca64c7674defd914bb526970c7f249e69e22a
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-11-21 19:20:03 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  ownerNumber: ['94773673969'], // Replace with your phone number
+  prefix: '!', // Bot prefix
+  sessionId: './auth_info_baileys', // Path to store session credentials
+  weatherApiKey: 'YOUR_API_KEY', // Add your weather API key
+  youtubeApiUrl: 'https://api.example.com/yt-download?url=', // Replace with valid YouTube API URL
+  exchangeRateApiUrl: 'https://api.exchangerate-api.com/v4/latest/', // Currency converter API URL
+};
